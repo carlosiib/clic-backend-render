7893fcab6ec8434d6e2354c529fee57b43f12d71
carlosiib <ntic.carlos@gmail.com>
2022-12-14 15:30:08 -0700

    env vars string

--- index.js
+++ index.js
@@ -35,6 +35,9 @@ const USER_URL = process.env.USER_URL;
 
 const ACTIVATION_URL = process.env.ACTIVATION_URL
 
+const ALTERNATE = process.env.ALTERNATE
+console.log("alternate", ALTERNATE)
+
 
 app.get('/', async (req, res) => {
   let arr = []
